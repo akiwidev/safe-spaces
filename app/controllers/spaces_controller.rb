80b4ede5eb8bd60fe977e29f5ca20e166209dd24
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-08-24 22:57:27 +0900

    change marker icons as test

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -9,10 +9,10 @@ class SpacesController < ApplicationController
       {
         lat: space.latitude,
         lng: space.longitude, #,
+        icon: "https://icons-for-free.com/iconfiles/png/512/map+marker+icon-1320166582858325800.png",
         infoWindow: { content: render_to_string(partial: "/spaces/info_window", locals: { space: space }) },
         # Uncomment the above line if you want each of your markers to display a info window when clicked
         # (you will also need to create the partial "/flats/map_box")
-        icon: space.photo
       }
     end
   end
