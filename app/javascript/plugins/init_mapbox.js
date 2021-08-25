4721a0a7db77c4126140687b90c641b5d52ac54d
Seira Yun <discoverdeepseira@gmail.com>
2021-08-25 18:37:05 +0900

    incident map added

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,6 +1,7 @@
 import mapboxgl from 'mapbox-gl';
 window.mapboxgl = mapboxgl;
 import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions'
+window.MapboxDirections = MapboxDirections
 import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css'
 import 'mapbox-gl/dist/mapbox-gl.css';
 // const MapboxDirections = require('@mapbox/mapbox-gl-directions');
