3ca4a52b43166e0d5b2522ea5309a4187a5e4fe4
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-08-25 12:57:56 +0900

    user pictures can now be seen on map

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -8,9 +8,9 @@ class SpacesController < ApplicationController
     @markers = @spaces_location.map do |space|
       {
         lat: space.latitude,
-        lng: space.longitude, #,
-        #icon: "https://icons-for-free.com/iconfiles/png/512/map+marker+icon-1320166582858325800.png",
-        infoWindow: { content: render_to_string(partial: "/spaces/info_window", locals: { space: space }) },
+        lng: space.longitude,
+        icon: Cloudinary::Utils.cloudinary_url(space.user.photo.key, { width: 50, height: 50, crop: :fill, radius: :max }),
+        infoWindow: { content: render_to_string(partial: "/spaces/info_window", locals: { space: space }) }
         # Uncomment the above line if you want each of your markers to display a info window when clicked
         # (you will also need to create the partial "/flats/map_box")
       }
