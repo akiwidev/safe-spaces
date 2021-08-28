96a7000d12bafdd9c4e70e1f79eca646f2262596
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-08-28 13:35:54 +0900

    refactored markers code

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -4,16 +4,7 @@ class SpacesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
-    @spaces = policy_scope(Space)
-    @spaces_location = Space.where.not(latitude: nil, longitude: nil)
-    @markers = @spaces_location.map do |space|
-      {
-        lat: space.latitude,
-        lng: space.longitude,
-        info_window: render_to_string(partial: "/spaces/info_window", locals: { space: space }),
-        image_url: helpers.asset_url(Cloudinary::Utils.cloudinary_url(space.user.photo.key))
-      }
-    end
+    set_space_markers
     @incident = Incident.new
   end
 
@@ -44,16 +35,7 @@ class SpacesController < ApplicationController
         image_url: helpers.asset_url(Cloudinary::Utils.cloudinary_url(@space.user.photo.key))
       }
     ]
-    @safe_spaces = policy_scope(Space)
-    @safe_spaces_location = Space.where.not(latitude: nil, longitude: nil, user: current_user)
-    @safe_spaces_markers = @safe_spaces_location.map do |space|
-      {
-        lat: space.latitude,
-        lng: space.longitude,
-        info_window: render_to_string(partial: "/spaces/info_window", locals: { space: space }),
-        image_url: helpers.asset_url(Cloudinary::Utils.cloudinary_url(space.user.photo.key))
-      }
-    end
+    set_space_markers
     @incident = Incident.new
   end
 
@@ -78,6 +60,19 @@ class SpacesController < ApplicationController
     unless signed_in?
       flash[:error] = "You must be logged in to access this section"
       redirect_to new_user_session_url # halts request cycle
+    end
+  end
+
+  def set_space_markers
+    @spaces = policy_scope(Space)
+    @spaces_location = Space.where.not(latitude: nil, longitude: nil)
+    @space_markers = @spaces_location.map do |space|
+      {
+        lat: space.latitude,
+        lng: space.longitude,
+        info_window: render_to_string(partial: "/spaces/info_window", locals: { space: space }),
+        image_url: helpers.asset_url(Cloudinary::Utils.cloudinary_url(space.user.photo.key))
+      }
     end
   end
 end
