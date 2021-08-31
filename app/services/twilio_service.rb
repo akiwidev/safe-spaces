3182bef5dc89702d55219ce0283fe1f50e2ac19f
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-08-31 15:53:13 +0900

    fixed twilio problem - maybe?

--- app/services/twilio_service.rb
+++ app/services/twilio_service.rb
@@ -11,12 +11,12 @@ class TwilioService
 
     # set up a client to talk to the Twilio REST API
     @client = Twilio::REST::Client.new(account_sid, auth_token)
-    @user =
+    p url = "#{ENV["DOMAIN"]}/connect/+817040990983"
     call = @client.calls.create(
         to: @phone_number,
         from: "+14433314397",
         # url: "http://demo.twilio.com/docs/voice.xml")
-        url: "#{ENV["DOMAIN"]}/connect/+817040990983")
+        url: url)
     puts call.to
   end
 end
