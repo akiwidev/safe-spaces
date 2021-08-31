72a337ba81d1f5b5f87af680d4830ffeb34d579e
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-08-31 19:30:03 +0900

    got call feature working

---
+++ app/controllers/twilio_controller.rb
@@ -0,0 +1,18 @@
+class TwilioController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:connect, :call]
+  skip_forgery_protection
+  skip_after_action :verify_authorized, only: [:connect, :call]
+
+  def call
+    @incident = Incident.find(params[:id])
+    TwilioService.new(sender: @incident.user.phone_num, receiver: @incident.space.user.phone_num).call
+  end
+
+  def connect
+    response = Twilio::TwiML::VoiceResponse.new do |r|
+      r.say(message: 'Connecting. One moment please.', voice: 'alice')
+      r.dial number: params[:phone_number]
+    end
+    render xml: response.to_s
+  end
+end
--- app/services/twilio_service.rb
+++ app/services/twilio_service.rb
@@ -1,22 +1,26 @@
 require 'twilio-ruby'
 
 class TwilioService
-  def initialize(phone_number)
-    @phone_number = phone_number
+  def initialize(attributes = {})
+    @receiver = attributes[:receiver]
+    @sender = attributes[:sender]
   end
+
   def call
     # Get your Account Sid and Auth Token from twilio.com/console
     account_sid = ENV['TWILIO_ACCOUNT_SID']
+    p account_sid
     auth_token = ENV['TWILIO_AUTH_TOKEN']
+    p auth_token
 
     # set up a client to talk to the Twilio REST API
     @client = Twilio::REST::Client.new(account_sid, auth_token)
-    p url = "#{ENV["DOMAIN"]}/connect/+817040990983"
+    p url = "#{ENV['DOMAIN']}/connect/#{@sender}"
     call = @client.calls.create(
-        to: @phone_number,
-        from: "+14433314397",
-        # url: "http://demo.twilio.com/docs/voice.xml")
-        url: url)
+      to: @receiver,
+      from: "+14433314397",
+      url: url
+    )
     puts call.to
   end
 end
