46a28ea8f544d32809e65f0111abe6c065a55565
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-08-31 12:30:37 +0900

    created TwilioService class

---
+++ app/services/twilio_service.rb
@@ -0,0 +1,18 @@
+require 'twilio-ruby'
+
+class TwilioService
+  # Get your Account Sid and Auth Token from twilio.com/console
+  def call
+    account_sid = ENV['TWILIO_ACCOUNT_SID']
+    auth_token = ENV['TWILIO_AUTH_TOKEN']
+
+    # set up a client to talk to the Twilio REST API
+    @client = Twilio::REST::Client.new(account_sid, auth_token)
+
+    call = @client.calls.create(
+        to: "+817040990983",
+        from: "+14433314397",
+        url: "http://demo.twilio.com/docs/voice.xml")
+    puts call.to
+  end
+end
