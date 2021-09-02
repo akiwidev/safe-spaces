4a0ed9bd0c858cf15a792aebe4c5dd8b4c3070a1
Seira Yun <discoverdeepseira@gmail.com>
2021-09-02 09:23:56 +0900

    fixed notification counter on navbar

--- app/javascript/channels/notification_channel.js
+++ app/javascript/channels/notification_channel.js
@@ -2,9 +2,17 @@ import consumer from "./consumer";
 
 const initNotificationCable = () => {
   const notificationsContainer = document.getElementById('notification-container');
-  const notificationContent = document.querySelector('.notification-content');
+  // const notificationContent = document.querySelector('.notification-content');
 
-  console.log(notificationsContainer);
+  consumer.subscriptions.create("Noticed::NotificationChannel", {
+    connected() {
+      // Called when the subscription is ready for use on the server
+    },
+    received(data) {
+      const unread = document.getElementById('unread');
+      if (unread) { unread.innerText = parseInt(unread.innerText, 10) + 1 }
+    },
+  });
   if (notificationsContainer) {
     // const id = notificationsContainer.dataset.notificationId;
     consumer.subscriptions.create("Noticed::NotificationChannel", {
@@ -15,8 +23,6 @@ const initNotificationCable = () => {
         console.log(data.notification);
         notificationsContainer.insertAdjacentHTML('afterbegin', data.notification);
         // notificationsContainer.scroll(0, notificationsContainer.scrollHeight);
-        const unread = document.getElementById('unread');
-        if (unread) { unread.innerText = parseInt(unread.innerText, 10) + 1 }
       },
     });
   }
