d236a7f753f810fb0f5bb1f3311809b93a380bf3
Seira Yun <discoverdeepseira@gmail.com>
2021-09-02 18:27:02 +0900

    added insert new notification via action cable for spaces notification

--- app/javascript/channels/notification_channel.js
+++ app/javascript/channels/notification_channel.js
@@ -21,16 +21,17 @@ const initNotificationCable = () => {
         // Called when the subscription is ready for use on the server
       },
       received(data) {
-        // console.log(data);
-        // notificationsContainer.insertAdjacentHTML('beforebegin', `
-        //   <div id= "notification-container">
-        //     <div class="notification-box mb-3">
-        //     </div>
-        //     <div class="notification-content">
-        //       <p>Someone is about to start a trip. Are you at home? If not, make your place unavailable</p>
-        //     </div>
-        //   </div>
-        // `);
+        console.log(data);
+        notificationsContainer.insertAdjacentHTML('beforebegin', `
+          <div id= "notification-container">
+            <div class="notification-box mb-3">
+              <img src="images/placeholder2.jpg" alt="placeholder profile photo">
+              <div class="notification-content">
+                <p style="color: white">Someone is about to start a trip. Are you at home? If not, make your place unavailable ${data.notification}</p>
+              </div>
+            </div>
+          </div>
+        `);
           // ${ cl_image_tag(data.space.user.photo.key, alt: "profile photo")  }
         // notificationsContainer.scroll(0, notificationsContainer.scrollHeight);
       },
